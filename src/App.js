2c2711f8158cd25d19ed5d5016a40a8fe8036495
Chester <changliuchangliu@hotmail.com>
2022-03-07 23:24:13 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Info />
+      <p>Chang Liu's App</p>
+      <AddItem />
+    </div>
+  );
+}
+
+function Info(){
+  const title = "This app is still in progress.";
+  return(
+    <div>
+      {title}
+      <h1>Inventory System</h1>
+      <p>Manage your stuff.</p>
+      </div>
+  )
+}
+
+function AddItem(){
+  return(
+    <div>
+      <label for="text-form">Type somthing: </label>
+      <input type="text" id = "text-form"/>
+    </div>
+  )
+}
+export default App;
